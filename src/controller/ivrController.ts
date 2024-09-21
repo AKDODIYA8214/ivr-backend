2985e6acf63ea8a89aac88252e5e768f76f79b0f
Akdodiya <aniket.dodiya@agami-tech.com>
2024-09-21 14:56:19 +0530

    service layer added

--- src/controller/ivrController.ts
+++ src/controller/ivrController.ts
@@ -1,2 +1,11 @@
 import { Request, Response } from "express";
-export const getAllIvr = (req: Request, res: Response) => {};
+import { getAllIvrService, getAllIvrPaginationService } from "../service/ivrService";
+export const getAllIvr = async (req: Request, res: Response) => {
+  try {
+    const { limit, offset } = req.params;
+    const data = limit && offset ? await getAllIvrPaginationService(limit, offset) : await getAllIvrService();
+    res.json(data);
+  } catch {
+    res.status(500).json({ error: "Server Error" });
+  }
+};
---
+++ src/service/ivrService.ts
@@ -0,0 +1,10 @@
+export const getAllIvrPaginationService = async (limit: string, offset: string) => {
+  const data: any[] = [];
+  const count = 0;
+  return { data: data, count: count };
+};
+export const getAllIvrService = async () => {
+  const data: any[] = [];
+  const count = 0;
+  return { data: data, count: count };
+};
